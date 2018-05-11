492ddb9b9573ea06d264a57e4d35b422132a09b9
elitemagician <isles4life+1@gmail.com>
2018-05-10 22:28:37 -0400

    Quick change to title of page

--- sports/views.py
+++ sports/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1><marquee><b>Sports News and Scores</marquee></b>")
+    return HttpResponse("<h1><marquee><b>CJ's Sports News and Scores</marquee></b>")
